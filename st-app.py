2ee886426a97d1c07940678675a0009584d53368
sraj-whatdoesthedatasay <shashankmr_75@yahoo.com>
2021-04-18 18:22:40 -0400

    new commit with st file

--- st-app.py
+++ st-app.py
@@ -44,8 +44,8 @@ if page == 'Hotdog or not':
         else:
             st.write('Its definitely not a hotdog')
 
-        st.write(**'The solution here is developed using Convolution Neural Network trained on ~1500 pictures of hot-dogs and of not-hot-dogs!**')
-        st.write(**'Expected accuracy is ~70% given the limitations of the model-size that can be placed on Git-Hub as well as the available training data**')
+        st.write('**The solution here is developed using Convolution Neural Network trained on ~1500 pictures of hot-dogs and of not-hot-dogs!**')
+        st.write('**Expected accuracy is ~70% given the limitations of the model-size that can be placed on Git-Hub as well as the available training data**')
 
         #uploaded_file = st.file_uploader("Choose an image...", type="jpg")
     # if uploaded_file is not None:
